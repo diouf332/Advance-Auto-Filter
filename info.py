8d1bafa3af456e4213035a78aa0d5042269b03ad
diouf332 <iandiouf8@gmail.com>
2025-07-01 11:31:35 +0300

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://iboxbot1:1111@cluster0.ndunpsw.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "vjbotsaban")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjbots')
+DATABASE_NAME = environ.get('DATABASE_NAME', "diouf")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'meandmyc')
 
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'moneykamalo.com')
